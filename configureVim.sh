62eff0c76a0b6ade2fe1b3e52649101547ae840c
Zimbu <ruler_of_heck@hotmail.com>
2019-06-22 21:39:32 -0600

    VIM configuration backs up the original vimrc and writes a very basic vimrc.

--- configureVim.sh
+++ configureVim.sh
@@ -1,2 +1,30 @@
 #!/bin/bash
 
+
+##############################
+# Backup the original .vimrc #
+##############################
+if [ ! -f ~/.vimrc.bak ] && [ -f ~/.vimrc ]; then
+  cp ~/.vimrc ~/.vimrc.bak
+elif [ ! -f ~/.vimrc.bak ] && [ ! -f ~/.vimrc ]; then
+  touch ~/.vimrc.bak
+fi
+
+################
+# Write .vimrc #
+################
+cat >~/.vimrc <<EOL
+
+" Leader key is the spacebar, local-leader key is the '\' key
+let mapleader = "\<space>"
+let maplocalleader = "\\\\"
+
+" Exit Insert Mode by hitting jk
+inoremap jk <esc>
+
+" Remaps H to beginning of line, L to end of line
+nnoremap H ^
+nnoremap L $
+
+EOL
+echo ".vimrc configuration written"
